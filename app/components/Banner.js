2a7b998150a2a9c098b4841ad4be224fc11cbbcf
Leejoongjae123 <ljj3347@naver.com>
2025-01-15 20:10:16 +0900

    '123'

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -88,7 +88,7 @@ export default function Banner() {
 
         </div>
       </div>
-      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-[+150%] flex space-x-0 bg-black w-1/2 h-24 z-10">
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 flex space-x-0 bg-black w-1/2 h-24 z-10">
         <button
           className="bg-black text-white px-4 py-2 rounded-l w-1/3 text-[25px]"
           onClick={() => setSelectMode("mode1")}
