40a4550be5334b7bc640543ed56c0ede10d0089c
Leejoongjae123 <ljj3347@naver.com>
2025-01-23 20:39:56 +0900

    '123'

--- app/divingtours/reservation/[id]/components/RequestForm.js
+++ app/divingtours/reservation/[id]/components/RequestForm.js
@@ -3,11 +3,37 @@
 import React from "react";
 import { Input, Select, SelectItem } from "@heroui/react";
 import { cn } from "@heroui/react";
-
+import { ScrollShadow } from "@heroui/react";
+import { Divider } from "@heroui/react";
 import companyTypes from "./company-types";
 import states from "./states";
 import companyIndustries from "./company-industries";
+import { RadioGroup, Radio } from "@heroui/react";
+import { Button } from "@heroui/react";
 
+export const Content = () => (
+  <div>
+    <p>□개인정보의 수집 ·이용에 관한 일반사항</p>
+    <p>○ 수집 ·이용 목적: 이용 대상자 확인 및 자격여부 확인</p>
+    <p>○ 수집 ·이용 할 개인정보의 항목 : 성명, 생년월일, 휴대폰 번호 등</p>
+    <p>○ 개인정보의 보유 및 이용기간 : 1년</p>
+    <br />
+    <p>□동의하지 않을 권리 및 미동의시 불이익</p>
+    <p>
+      ○다이빙 투어 신청자는 개인정보의 수집 ·이용에 대한 동의를 거부할 권리가
+      있으나 동의하지 않을 경우 다이빙 투어 신청 접수가 거부될 수 있습니다.
+    </p>
+    <br />
+    <p>
+      본인은 BDN 필리핀 코론 수중 프로필 촬영 다이빙 투어를 신청함에 있어 상기
+      내용에 대하여 충분히 인지 하였으며, 기관의 개인정보 수집 및 이용, 준수사항
+      및 동의서 일체에 동의 합니다.
+    </p>
+    <br />
+    <br />
+    <p>위 내용에 모두 동의하시겠습니까?</p>
+  </div>
+);
 const CompanyInformationForm = React.forwardRef(
   ({ className, ...props }, ref) => {
     const inputProps = {
@@ -27,11 +53,11 @@ const CompanyInformationForm = React.forwardRef(
     };
 
     return (
-      <>
+      <div className="w-[90vw] md:w-[60vw] flex flex-col items-center justify-center gap-y-12">
         <div className="text-4xl font-bold leading-9 text-default-foreground">
           ‘필리핀 코론 수중 프로필 촬영 다이빙 투어’{" "}
         </div>
-        <div className="py-4 text-default-500 bg-gray-100 rounded-lg p-6">
+        <div className="py-4 text-default-500 bg-gray-100 rounded-lg p-6 w-full">
           <p>※ 필리핀 코론 수중 프로필 촬영 다이빙 투어 신청서 작성 안내</p>
           <p>
             {" "}
@@ -48,7 +74,7 @@ const CompanyInformationForm = React.forwardRef(
         <form
           ref={ref}
           className={cn(
-            "flex grid grid-cols-12 flex-col gap-4 py-8",
+            "flex grid grid-cols-12 flex-col gap-4 py-8 w-full",
             className
           )}
           {...props}
@@ -73,83 +99,83 @@ const CompanyInformationForm = React.forwardRef(
 
           <Select
             classNames={{
-              base: "col-span-6 ",
+              base: "col-span-12 md:col-span-6 ",
             }}
             label="성별"
             name="gender"
             placeholder="성별을 선택해주세요"
             labelPlacement="outside"
-            selectedKeys={['male']}
+            selectedKeys={["male"]}
             isRequired
           >
-            <SelectItem key="male" value="male">남</SelectItem>
-            <SelectItem key="female" value="female">여</SelectItem>
+            <SelectItem key="male" value="male">
+              남
+            </SelectItem>
+            <SelectItem key="female" value="female">
+              여
+            </SelectItem>
           </Select>
 
           <Input
             className="col-span-12 md:col-span-6"
-            label="Street Name"
-            name="street-name"
-            placeholder="Geary 2234"
+            label="나이(생년월일)"
+            name="age"
+            placeholder="생년월일을 입력해주세요"
+            isRequired
             {...inputProps}
           />
 
           <Input
             className="col-span-12 md:col-span-6"
-            label="Suite"
-            name="suite"
-            placeholder="#166"
-            {...inputProps}
-          />
-
-          <Select
-            className="col-span-12 md:col-span-4"
-            items={states}
-            label="State"
-            name="state"
-            placeholder="Delaware"
-            {...selectProps}
-          >
-            {(registrationState, index) => (
-              <SelectItem key={`${registrationState.value}-${index}`}>
-                {registrationState.title}
-              </SelectItem>
-            )}
-          </Select>
-
-          <Input
-            className="col-span-12 md:col-span-4"
-            label="City"
-            name="city"
-            placeholder="San Francisco"
+            label="지역(서울,경기,대전 등)"
+            name="location"
+            placeholder="지역을 입력해주세요"
+            isRequired
             {...inputProps}
           />
 
           <Input
-            className="col-span-12 md:col-span-4"
-            label="Zip Code"
-            name="zip-code"
-            placeholder="9409"
+            className="col-span-12 md:col-span-12"
+            label="보유한 라이센스(보유한 라이센스가 없으신분들은 '없음'이라고 적어주시면 됩니다."
+            name="license"
+            placeholder="보유한 라이센스를 입력해주세요"
+            isRequired
             {...inputProps}
           />
 
           <Input
             className="col-span-12 md:col-span-6"
-            label="EIN"
-            name="ein"
-            placeholder="Type your company EIN here"
+            label="통화가능시간"
+            name="call-time"
+            placeholder="통화가능시간을 입력해주세요"
+            isRequired
             {...inputProps}
           />
-
           <Input
             className="col-span-12 md:col-span-6"
-            label="Confirm EIN"
-            name="confirm-ein"
-            placeholder="Confirm your company EIN here"
+            label="이메일(선택)"
+            name="email"
+            placeholder="이메일을 입력해주세요"
             {...inputProps}
           />
         </form>
-      </>
+        <div className="w-full flex flex-col items-center justify-center gap-y-4">
+          <div className="text-3xl font-bold">개인정보 수집 · 이용 동의서</div>
+          <Divider className="w-full" />
+          <ScrollShadow hideScrollBar className="w-full max-h-[50vh]">
+            <Content />
+          </ScrollShadow>
+          <RadioGroup orientation="horizontal">
+            <Radio value="agree">동의함</Radio>
+            <Radio value="disagree">동의하지 않음</Radio>
+          </RadioGroup>
+        </div>
+        <div className="w-full flex flex-col items-center justify-end gap-y-4 my-4">
+          <Button color="primary" className="w-full">
+            신청하기
+          </Button>
+        </div>
+      </div>
     );
   }
 );
