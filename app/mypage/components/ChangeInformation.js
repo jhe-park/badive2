f96827233d25ae13a7af2598bd5bf91ae97e1207
Leejoongjae123 <ljj3347@naver.com>
2025-02-02 20:20:22 +0900

    123

--- app/mypage/components/ChangeInformation.js
+++ app/mypage/components/ChangeInformation.js
@@ -24,121 +24,139 @@ export default function App() {
         setAction(`submit ${JSON.stringify(data)}`);
       }}
     >
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-5 w-full">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-5 w-full gap-y-5">
         <div className="flex flex-col">
-          <div>비밀번호</div>
+          {/* <div>비밀번호</div> */}
           <Input
+            label="비밀번호"
             name="username"
-            placeholder=""
+            placeholder="비밀번호"
+            variant="bordered"
             type="text"
             className="w-full col-span-1"
           />
         </div>
         <div className="flex flex-col">
-          <div>비밀번호 확인</div>
+          {/* <div>비밀번호 확인</div> */}
           <Input
+            label="비밀번호 확인"
             name="username"
-            placeholder=""
+            placeholder="비밀번호 확인"
+            variant="bordered"
             type="text"
             className="w-full col-span-1"
           />
         </div>
         <div className="flex flex-col">
-          <div>이름</div>
+          {/* <div>이름</div> */}
           <Input
+            label="이름"
             name="username"
-            placeholder=""
+            placeholder="홍길동동"
             type="text"
+            variant="bordered"
             className="w-full col-span-1"
           />
         </div>
         <div className="flex flex-col">
-          <div>휴대폰번호</div>
+          {/* <div>휴대폰번호</div> */}
           <Input
+            label="휴대폰번호"
             name="username"
             placeholder="010-1234-5678"
             type="text"
+            variant="bordered"
             className="w-full col-span-1"
           />
         </div>
         <div className="flex flex-col">
-          <div>생년월일</div>
+          {/* <div>생년월일</div> */}
           <Input
+            label="생년월일"
             name="username"
             placeholder="1980-01-01"
             type="text"
+            variant="bordered"
             className="w-full col-span-1"
           />
         </div>
         <div className="flex flex-col">
-          <div>보유한 라이센스</div>
+          {/* <div>보유한 라이센스</div> */}
           <Input
+            label="보유한 라이센스"
             name="username"
             placeholder=""
             type="text"
+            variant="bordered"
             className="w-full col-span-1"
           />
         </div>
         <div className="flex flex-col gap-2">
-          <div>희망하는 강습</div>
+          {/* <div>희망하는 강습</div> */}
           <div className="flex flex-row gap-2 justify-start items-center w-full">
-            <span>01.</span>
-            <Select>
-              <SelectItem>강습1</SelectItem>
-              <SelectItem>강습2</SelectItem>
-              <SelectItem>강습3</SelectItem>
+            {/* <span>01.</span> */}
+            <Select label="희망하는 강습1" variant="bordered">
+              <SelectItem value="강습1">강습1</SelectItem>
+              <SelectItem value="강습2">강습2</SelectItem>
+              <SelectItem value="강습3">강습3</SelectItem>
             </Select>
           </div>
           <div className="flex flex-row gap-2 justify-start items-center w-full">
-            <span>02.</span>
-            <Select>
-              <SelectItem>강습1</SelectItem>
-              <SelectItem>강습2</SelectItem>
-              <SelectItem>강습3</SelectItem>
+            {/* <span>02.</span> */}
+            <Select label="희망하는 강습2" variant="bordered">
+              <SelectItem value="강습1">강습1</SelectItem>
+              <SelectItem value="강습2">강습2</SelectItem>
+              <SelectItem value="강습3">강습3</SelectItem>
             </Select>
           </div>
           <div className="flex flex-row gap-2 justify-start items-center w-full">
-            <span>03.</span>
-            <Select>
-              <SelectItem>강습1</SelectItem>
-              <SelectItem>강습2</SelectItem>
-              <SelectItem>강습3</SelectItem>
+            {/* <span>03.</span> */}
+            <Select label="희망하는 강습3" variant="bordered">
+              <SelectItem value="강습1">강습1</SelectItem>
+              <SelectItem value="강습2">강습2</SelectItem>
+              <SelectItem value="강습3">강습3</SelectItem>
             </Select>
           </div>
         </div>
         <div className="flex flex-col">
-          <div>성별</div>
-          <RadioGroup orientation="horizontal">
+          {/* <div>성별</div> */}
+          <RadioGroup label="성별" orientation="horizontal" className="w-full border-2 border-gray-200 rounded-xl p-2">
             <Radio value="male">남</Radio>
             <Radio value="female">여</Radio>
           </RadioGroup>
         </div>
         <div className="flex flex-col">
-          <div>이메일</div>
+          {/* <div>이메일</div> */}
           <Input
-            name="username"
+            label="이메일"
+            name="email"
             placeholder="abcd1234@naver.com"
             type="text"
+            variant="bordered"
             className="w-full col-span-1"
           />
         </div>
-        <div className="flex flex-col">
-          <div>주소</div>
+        <div className="flex flex-col gap-y-5">
+          {/* <div>주소</div> */}
           <div className="flex flex-row gap-2 justify-start items-center w-full">
             <Input
-              name="username"
-              placeholder="abcd1234@naver.com"
+              name="address"
+              label="주소"
+              placeholder="서울시시"
               type="text"
+              variant="bordered"
               className="w-full col-span-1"
             />
             <Button>주소검색</Button>
           </div>
           <div>
-            <div>상세주소</div>
+            {/* <div>상세주소</div> */}
             <Input
-              name="username"
-              placeholder="abcd1234@naver.com"
+              name="detailAddress"
+              placeholder="상세주소"
+              label="개포동동"
               type="text"
+              variant="bordered"
               className="w-full col-span-1"
             />
           </div>
