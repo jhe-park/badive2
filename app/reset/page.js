917998d6a319cb7f46e61e5b18c7b497af0635ec
Leejoongjae123 <ljj3347@naver.com>
2025-02-04 14:11:44 +0900

    123

--- app/reset/page.js
+++ app/reset/page.js
@@ -6,6 +6,7 @@ import { Input, Button } from "@heroui/react";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Reset() {
     const [password, setPassword] = useState("");
@@ -38,56 +39,58 @@ export default function Reset() {
     };
 
     return (
-        <div className="flex h-full  w-full flex-col items-center justify-center gap-y-10 my-32">
-            <div className="flex flex-col items-center gap-y-5">
-                <div>
-                    <p className="font-bold text-5xl">BDN DIVE</p>
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="flex h-full  w-full flex-col items-center justify-center gap-y-10 my-32">
+                <div className="flex flex-col items-center gap-y-5">
+                    <div>
+                        <p className="font-bold text-5xl">BDN DIVE</p>
+                    </div>
+                    <div>
+                        <p className="text-small text-default-500 text-[24px]">로그인</p>
+                    </div>
                 </div>
-                <div>
-                    <p className="text-small text-default-500 text-[24px]">로그인</p>
-                </div>
-            </div>
-            <div className="mt-2 flex w-[90%] md:w-1/3 flex-col gap-4 rounded-large bg-content1 px-8 py-6 border-2 border-gray-300">
-                <div className="flex flex-col gap-3">
-                    <Input
-                        isRequired
-                        label="비밀번호"
-                        name="password"
-                        placeholder="비밀번호를 입력해주세요"
-                        type="password"
-                        variant="bordered"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        isInvalid={password !== passwordCheck}
-                    />
-                    <Input
-                        type="password"
+                <div className="mt-2 flex w-[90%] md:w-1/3 flex-col gap-4 rounded-large bg-content1 px-8 py-6 border-2 border-gray-300">
+                    <div className="flex flex-col gap-3">
+                        <Input
+                            isRequired
+                            label="비밀번호"
+                            name="password"
+                            placeholder="비밀번호를 입력해주세요"
+                            type="password"
+                            variant="bordered"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            isInvalid={password !== passwordCheck}
+                        />
+                        <Input
+                            type="password"
 
-                        isRequired
-                        label="비밀번호 확인"
-                        name="password"
-                        placeholder="비밀번호를 입력해주세요"
-                        variant="bordered"
-                        value={passwordCheck}
-                        onChange={(e) => setPasswordCheck(e.target.value)}
-                        isInvalid={password !== passwordCheck}
-                    />
-                    {password !== passwordCheck && <p className="text-small text-default-500 text-[14px]">비밀번호가 일치하지 않습니다.</p>}
-                    <Button isDisabled={password !== passwordCheck} className="w-full" color="primary" type="submit" onPress={handlePasswordChange}>
-                        비밀번호 변경
+                            isRequired
+                            label="비밀번호 확인"
+                            name="password"
+                            placeholder="비밀번호를 입력해주세요"
+                            variant="bordered"
+                            value={passwordCheck}
+                            onChange={(e) => setPasswordCheck(e.target.value)}
+                            isInvalid={password !== passwordCheck}
+                        />
+                        {password !== passwordCheck && <p className="text-small text-default-500 text-[14px]">비밀번호가 일치하지 않습니다.</p>}
+                        <Button isDisabled={password !== passwordCheck} className="w-full" color="primary" type="submit" onPress={handlePasswordChange}>
+                            비밀번호 변경
 
 
-                    </Button>
-                    <Link href="/login" className="w-full">
-                        <Button variant="bordered" className="w-full" color="primary" type="button">
-                            로그인으로 이동
                         </Button>
+                        <Link href="/login" className="w-full">
+                            <Button variant="bordered" className="w-full" color="primary" type="button">
+                                로그인으로 이동
+                            </Button>
 
-                    </Link>
+                        </Link>
+                    </div>
+
+
                 </div>
-
-
             </div>
-        </div>
+        </Suspense>
     );
 }
