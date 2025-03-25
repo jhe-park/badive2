c03ea7e163af6c4ee2822c9bba9da64d93c20b0b
Leejoongjae123 <ljj3347@naver.com>
2025-03-25 18:35:18 +0900

    123

--- app/components/Curriculum.js
+++ app/components/Curriculum.js
@@ -2,34 +2,32 @@
 import React from "react";
 import Link from "next/link";
 import SlideUp from "@/components/animation/SlideUp";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 export default function Curriculum() {
   const router = useRouter();
   return (
-    <div className="w-full h-full md:max-w-[1280px] xl:aspect-[1280/696] md:aspect-[768/545] aspect-[375/290] flex flex-col justify-evenly items-start ">
+    <div className="w-full  md:max-w-[1280px] h-auto flex flex-col justify-evenly items-start gap-y-6">
       <div className="flex gap-x-5 justify-center xl:justify-start items-center w-full">
+        <Image src="/logo/logo.png" alt="logo" width={75} height={50} className="md:w-[75px] md:h-[50px] w-[50px] h-[25px]" />
         <p className="xl:text-[36px] md:text-[30px] text-[20px] font-bold">BADIVE 교육과정</p>
       </div>
       <SlideUp>
-        <div className="w-full grid grid-cols-3 gap-2 px-2">
+        <div className="w-full h-full grid grid-cols-2 gap-2 px-2">
           {[
-            { src: "curriculum1.png", title: "스쿠버다이빙",url:"/programs/scuberdiving" },
-            { src: "curriculum2.png", title: "프리다이빙",url:"/programs/freediving" },
-            { src: "curriculum3.png", title: "머메이드",url:"/programs/mermaid" },
+            { src: "program1.png", title: "스쿠버다이빙", url:"/programs/scuberdiving" },
+            { src: "program2.png", title: "프리다이빙", url:"/programs/freediving" },
+            { src: "program3.png", title: "머메이드", url:"/programs/mermaid" },
+            { src: "program4.png", title: "언더워터", url:"/programs/underwater" },
           ].map((item, index) => (
-            <div key={index} className="relative w-full flex justify-center items-center group">
-              <div className="w-full overflow-hidden rounded-3xl aspect-[115/200] md:aspect-[238/400] xl:aspect-[393/550] md:max-w-[393px]">
+            <div key={index} className="relative aspect-square w-full flex justify-center items-center group">
+              <div className="w-full h-full overflow-hidden rounded-3xl">
                 <img
-                  src={`/curriculum/${item.src}`}
+                  src={`/banner/${item.src}`}
                   alt={item.title}
                   className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-110 cursor-pointer"
-                  onClick={() => router.push(`/curriculum/${item.title}`)}
+                  onClick={() => router.push(item.url)}
                 />
-                <div className="absolute inset-0 flex items-center justify-center cursor-pointer" onClick={() => router.push(item.url)}>
-                  <div className="text-sm md:text-[32px] font-bold absolute w-full h-12 md:h-36 bg-black/75 text-white flex justify-center items-center opacity-0 transition-all duration-500 group-hover:opacity-100 max-w-[393px]">
-                    {item.title}
-                  </div>
-                </div>
               </div>
             </div>
           ))}
--- app/components/Experts.js
+++ app/components/Experts.js
@@ -1,60 +1,20 @@
 import React from "react";
 import SlideUp from "@/components/animation/SlideUp";
 import Link from "next/link";
+import Image from "next/image";
 export default function Experts() {
   return (
     <div className="w-full xl:max-w-[1280px] h-full md:h-auto xl:aspect-[1280/754] md:aspect-[768/552] aspect-[375/314] flex flex-col justify-evenly items-start md:my-0 px-1 md:px-0">
       <div className="flex gap-x-5 justify-center xl:justify-start items-center w-full">
+        <Image src="/logo/logo.png" alt="Expert 1" width={75} height={50} className="md:w-[75px] md:h-[50px] w-[50px] h-[25px]" />
         <p className=" text-[20px] md:text-[30px] xl:text-[35px] font-bold text-center md:text-start">BADIVE 소속강사</p>
       </div>
       <SlideUp>
-      <div className="w-full md:aspect-[758/407] aspect-[375/222]  flex gap-2">
-        <div className="w-1/3 h-full flex flex-col justify-center items-center gap-2">
-          <div className="w-full h-full bg-green-500 overflow-hidden">
-            <img
-              src="/experts/experts1.png"
-              alt="Expert 1"
-              className="w-full h-full transition-transform duration-300 transform hover:scale-110"
-            />
-          </div>
-          <div className="w-full h-full bg-yellow-500 overflow-hidden">
-            <img
-              src="/experts/experts2.png"
-              alt="Expert 2"
-              className="w-full h-full transition-transform duration-300 transform hover:scale-110"
-            />
-          </div>
-        </div>
-        <div className="w-1/3 h-full bg-green-100 flex justify-center items-center relative gap-2">
-          <div className="w-full h-full bg-green-500 overflow-hidden relative group">
-            <img
-              src="/experts/experts3.png"
-              alt="Expert 3"
-              className="w-full h-full transition-transform duration-300 transform hover:scale-110"
-            />
-            <Link href="/instructors/bdn">
-            <div className="text-sm md:text-[32px] font-bold absolute inset-x-0 top-1/2 transform -translate-y-1/2 h-12 md:h-36 bg-black bg-opacity-75 text-white flex justify-center items-center opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-              강사소개
-            </div>
-            </Link>
-          </div>
-        </div>
-        <div className="w-1/3 h-full flex flex-col justify-center items-center gap-2">
-          <div className="w-full h-full bg-green-500 overflow-hidden">
-            <img
-              src="/experts/experts4.png"
-              alt="Expert 3"
-              className="w-full h-full transition-transform duration-300 transform hover:scale-110"
-            />
-          </div>
-          <div className="w-full h-full bg-yellow-500 overflow-hidden">
-            <img
-              src="/experts/experts5.png"
-              alt="Expert 4"
-              className="w-full h-full transition-transform duration-300 transform hover:scale-110"
-            />
-          </div>
-        </div>
+      
+      <div className="w-full md:aspect-[758/407] aspect-[375/222]  flex gap-2 relative">
+        <Link href="/instructors/bdn">
+        <Image src="/banner/expert.png" alt="Expert 1" fill className="object-cover" />
+        </Link>
       </div>
       </SlideUp>
     </div>
