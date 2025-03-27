e4ccb060d647467f9b480f8bc4343cf49bf8d9af
EunJin Kim <eunjin9639@gmail.com>
2025-03-27 23:37:54 +0900

    메인 배너 컴포넌트 개선 및 애니메이션 추가, 새로운 이미지와 비디오 파일 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,10 +27,21 @@ const config = {
             opacity: '1',
             transform: 'scale(1)'
           },
+        },
+        'slide-in-bottom': {
+          '0%': {
+            transform: 'translateY(100px)',
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: '1',
+          }
         }
       },
       animation: {
-        'scale-fade-in': 'scale-fade-in 0.5s ease-out forwards'
+        'scale-fade-in': 'scale-fade-in 0.5s ease-out forwards',
+        'slide-in-bottom': 'slide-in-bottom 0.8s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
       },
       screens: {
         sm: '768px',    // 태블릿
