3b102b9fa4ae25402542d70aebf0a50d2ff9dbd5
EunJin Kim <eunjin9639@gmail.com>
2025-03-29 23:25:54 +0900

    MainSectionHeader 컴포넌트의 폰트 클래스 이름 수정

--- app/components/MainSectionHeader.js
+++ app/components/MainSectionHeader.js
@@ -1,5 +1,5 @@
 const MainSectionHeader = ({title}) => {
-  return (<h1 className='flex items-center justify-center font-ELAND_Choice_M font-bold 
+  return (<h1 className='flex items-center justify-center font-eland font-bold 
     gap-[10px] text-3xl py-[50px]
     sm:gap-[14px] sm:text-[40px] sm:py-24
     md:gap-[27px] md:text-[50px] md:py-[115px]
