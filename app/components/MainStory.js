47a0f9c1b22badf3505a52d32e362aa9df52b7ed
EunJin Kim <eunjin9639@gmail.com>
2025-03-31 20:11:31 +0900

    MainStory 컴포넌트에 비디오 모달 추가 및 전체 화면 기능 구현

--- app/components/MainStory.js
+++ app/components/MainStory.js
@@ -1,7 +1,10 @@
-'use client'
+'use client';
 
+import { useEffect, useRef, useState } from 'react'
 import MainSectionHeader from './MainSectionHeader'
 import { useImageAnimation } from '../../hooks/useAnimation'
+import VideoModal from './VideoModal'
+
 const MainStory = () => {
   const THUMBNAILS = [
     {
@@ -24,15 +27,43 @@ const MainStory = () => {
 
   const { containerRef } = useImageAnimation('thumbnail')
 
+  const [selected, setSelected] = useState('')
+  const [isFullScreen, setIsFullScreen] = useState('')
+  const [isClient, setIsClient] = useState(false)
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  const toggleFullScreen = () => {
+    if (!isFullScreen) {
+      modalRef.current?.requestFullscreen?.()
+      modalRef.current?.webkitRequestFullscreen?.()
+      modalRef.current?.msRequestFullscreen?.()
+    } else {
+      document.exitFullscreen?.()
+      document.webkitExitFullscreen?.()
+      document.msExitFullscreen?.()
+    }
+    setIsFullScreen(!isFullScreen)
+  }
+
+  const closeModal = () => setSelected('')
+
+
   return (
-    <section className='bg-white' ref={containerRef}>
-      <MainSectionHeader title='BADIVE DIVING STORY' />
-      <div className='grid grid-cols-2 sm:grid-cols-4 w-full'>
-        {THUMBNAILS.map((item) => (<a key={item.img} href={item.link} target='_blank' className='thumbnail block overflow-hidden'>
-          <img alt={item.img} src={item.img} className='w-full h-auto transform transition-transform duration-300 ease-out' />
-        </a>))}
-      </div>
-    </section>
+    <>
+      <section className='bg-white' ref={containerRef}>
+        <MainSectionHeader title='BADIVE DIVING STORY' />
+        <div className='grid grid-cols-2 sm:grid-cols-4 w-full'>
+          {THUMBNAILS.map((item) => (<div key={item.img} onClick={() => setSelected(item.link)} className='thumbnail block overflow-hidden'>
+            <img alt={item.img} src={item.img} className='w-full h-auto transform transition-transform duration-300 ease-out' />
+          </div>))}
+        </div>
+      </section>
+      {selected && <VideoModal selected={selected} onClose={() => setSelected('')} />}
+    </>
   )
 }
 export default MainStory
