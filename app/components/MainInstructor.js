f9bc7e26ed0cdbb48d750a6f977444cf075aa2c5
EunJin Kim <eunjin9639@gmail.com>
2025-03-31 20:24:44 +0900

    MainInstructor 컴포넌트의 텍스트 줄바꿈 추가 및 이미지 클래스 순서 변경

--- app/components/MainInstructor.js
+++ app/components/MainInstructor.js
@@ -13,10 +13,10 @@ const MainInstructor = () => {
     <section className='bg-white'>
       <MainSectionHeader title='BADIVE 소속강사' />
       <div className='text-lg sm:text-3xl md:text-[35px] text-center transform transition-transform duration-300 ease-out' ref={textRef}>
-        <span className={textClass}>바다</span>를 사랑하는 전문가들이 <span className={textClass}>당신의 첫걸음을 함께</span>합니다
+        <span className={textClass}>바다</span>를 사랑하는<br/> 전문가들이 <span className={textClass}>당신의 첫걸음을 함께</span>합니다
       </div>
       <a className='mt-[47px] relative max-w-[1720px] block mx-auto' href='/instructors/bdn' ref={imageRef}>
-        <img src='/instructor/main_instructor.png' alt='main_instructor' className='transform transition-transform duration-300 ease-out' />
+        <img src='/instructor/main_instructor.png' alt='main_instructor' className='transition-transform duration-300 ease-out transform' />
         <ViewMoreButton className={'absolute z-10 bottom-2 right-[21px] sm:bottom-[15px] md:bottom-[21px] md:right-[23px] lg:bottom-[15px] lg:right-6'} useStroke />
       </a>
     </section>
