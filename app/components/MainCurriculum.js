b4aab83abd0e2efa0352878d44a3b896fccb7752
EunJin Kim <eunjin9639@gmail.com>
2025-04-01 20:03:49 +0900

    MainCurriculum 컴포넌트 스타일 수정: 여백 및 그리드 간격 조정

--- app/components/MainCurriculum.js
+++ app/components/MainCurriculum.js
@@ -34,10 +34,11 @@ const MainCurriculum = () => {
   const { containerRef } = useImageAnimation('curriculum')
 
   return (
-    <section className='bg-white'>
+    <section className='bg-white px-[15px] pb-[29px] sm:pb-[25px] lg:pb-11'>
       <MainSectionHeader title='BADIVE 교육프로그램' />
-      <div className='grid grid-cols-1 gap-5 sm:gap-0 sm:grid-cols-2 md:grid-cols-4' ref={containerRef}>
-        {CURRICULUMS.map((item, idx) => <a key={item.img} href={item.link} className='relative curriculum'>
+      <div className='grid grid-cols-1
+      gap-5 sm:gap-y-[11px] sm:gap-x-[9px] md:gap-x-[15px] sm:grid-cols-2 md:grid-cols-4' ref={containerRef}>
+        {CURRICULUMS.map((item, idx) => <a key={item.img} href={item.link} className='relative curriculum overflow-hidden rounded-[15px] sm:rounded-[30px]'>
           <img src={item.img} alt={item.link} className={['transform transition-transform ease-out', duration[idx]].join(' ')} />
           <ViewMoreButton className={'[&>span]:!text-white [&>span]:!border-white absolute z-10 bottom-2 right-[21px] sm:bottom-[15px] md:bottom-[21px] md:right-[23px] lg:bottom-[15px] lg:right-6'} />
         </a>
