6704f3abcc78152c7bbf177725bf29381e622805
EunJin Kim <eunjin9639@gmail.com>
2025-04-02 10:46:53 +0900

    Update MainSectionHeader.js

--- app/components/MainSectionHeader.js
+++ app/components/MainSectionHeader.js
@@ -7,14 +7,14 @@ const MainSectionHeader = ({ title }) => {
   
   return (<h1 ref={containerRef}
     className='font-freesentationVF text-black font-bold section-title flex items-center justify-center
-      gap-[10px] text-3xl py-[50px]
+      gap-[7px] text-[25px] py-[50px]
       sm:gap-[14px] sm:text-[40px] sm:py-24
       md:gap-[27px] md:text-[50px] md:py-[115px]
       lg:gap-[27px] lg:py-[100px]
       w-full h-auto transform transition-transform duration-300 ease-out
     '>
     <img src='/story/title.png' className='w-[50px] h-[50px]' />
-    <span className='whitespace-normal break-keep'>{title}<span>
+    {title}
   </h1>)
 }
 export default MainSectionHeader
