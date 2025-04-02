48b4a41663fc20297bd1a510003a080badfeb3d5
EunJin Kim <eunjin9639@gmail.com>
2025-04-02 11:06:46 +0900

    Update MainInstructor.js

--- app/components/MainInstructor.js
+++ app/components/MainInstructor.js
@@ -12,7 +12,7 @@ const MainInstructor = () => {
   return (
     <section className='bg-white'>
       <MainSectionHeader title='BADIVE 소속강사' />
-      <div className='text-nowrap text-[20px] sm:text-[40px] md:text-5xl text-center transform transition-transform duration-300 ease-out' ref={textRef}>
+      <div className='text-nowrap text-[20px] sm:text-[40px] md:text-5xl text-black text-center transform transition-transform duration-300 ease-out' ref={textRef}>
         <span className={textClass}>바다</span>를 사랑하는<br/> 전문가들이 <span className={textClass}>당신의 첫걸음을 함께</span>합니다
       </div>
       <a className='mt-[47px] relative max-w-[1720px] block mx-auto' href='/instructors/bdn' ref={imageRef}>
