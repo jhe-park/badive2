c6a3cba1c6d8cd953dce32699f3519b476bb3970
EunJin Kim <eunjin9639@gmail.com>
2025-04-02 02:42:51 +0000

    메인 스토리 수정

--- app/components/MainStory.js
+++ app/components/MainStory.js
@@ -62,7 +62,7 @@ const MainStory = () => {
             <button
               onClick={() => setSelected(item.link)} 
               className="absolute Z-10 top-0 left-0 w-full h-full inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white opacity-0 group-hover:opacity-100 transition-opacity"
-              aria-label={`Play ${image.title}`}
+              aria-label={`Play ${item.link}`}
             >
               <IoIosPlayCircle className="w-10 h-10 md:w-20 md:h-20" />
             </button>
