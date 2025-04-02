35b497bf5bb80307a8baa7689a82eb9e938c0201
EunJin Kim <eunjin9639@gmail.com>
2025-04-02 10:39:28 +0900

    Update MainCurriculum.js

--- app/components/MainCurriculum.js
+++ app/components/MainCurriculum.js
@@ -34,12 +34,12 @@ const MainCurriculum = () => {
   const { containerRef } = useImageAnimation('curriculum')
 
   return (
-    <section className='bg-white px-[15px] pb-[29px] sm:pb-[25px] lg:pb-11'>
+    <section className='bg-white px-[15px] pb-[29px] sm:pb-[25px] lg:pb-11 w-full'>
       <MainSectionHeader title='BADIVE 교육프로그램' />
       <div className='grid grid-cols-1
       gap-5 sm:gap-y-[11px] sm:gap-x-[9px] md:gap-x-[15px] sm:grid-cols-2 md:grid-cols-4' ref={containerRef}>
         {CURRICULUMS.map((item, idx) => <a key={item.img} href={item.link} className='relative curriculum overflow-hidden rounded-[15px] sm:rounded-[30px]'>
-          <img src={item.img} alt={item.link} className={['transform transition-transform ease-out', duration[idx]].join(' ')} />
+          <img src={item.img} alt={item.link} className={['transform transition-transform ease-out w-full', duration[idx]].join(' ')} />
           <ViewMoreButton className={'[&>span]:!text-white [&>span]:!border-white absolute z-10 bottom-2 right-[21px] sm:bottom-[15px] md:bottom-[21px] md:right-[23px] lg:bottom-[15px] lg:right-6'} />
         </a>
         )}
