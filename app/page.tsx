e8db1c6217c1dfa2f86f5357be290a749566a5f8
leekb <tmwlak@gmail.com>
2025-04-03 18:08:39 +0900

    Welcome popup 복구

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,13 @@ import MainStory from './components/MainStory.js'
 import MainInstructor from './components/MainInstructor.js'
 import MainCurriculum from './components/MainCurriculum.js'
 import MainNews from './components/MainNews.js'
+import WelcomePopup from "./components/WelcomePopup";
 
 export default async function Home() {
   return (
     <div className="flex w-full h-full flex-col items-center justify-start">
+      <WelcomePopup />
+
       <MainBanner />
       <MainStory />
       <MainInstructor />
