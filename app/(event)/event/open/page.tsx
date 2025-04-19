5289d5243e67e883f93a5695b3e3b98cabeb836a
Lee GUKBeom <tmwlak@gmail.com>
2025-04-19 09:59:51 +0900

    [fix] /event/open 페이지 : iframe을 redirect 로직으로 변경

--- app/(event)/event/open/page.tsx
+++ app/(event)/event/open/page.tsx
@@ -1,7 +1,10 @@
 import { type NextPage } from 'next';
+import { redirect } from 'next/navigation';
 
 const PageForEvent1: NextPage<NextPageProps> = ({}) => {
-  return <iframe src="http://jhecompany.dothome.co.kr/event/" className="w-screen min-h-screen" frameBorder="0"></iframe>;
+  redirect('http://jhecompany.dothome.co.kr/event');
+
+  return <iframe src="http://jhecompany.dothome.co.kr/event" className="w-screen min-h-screen" frameBorder="0"></iframe>;
 };
 
 export default PageForEvent1;
