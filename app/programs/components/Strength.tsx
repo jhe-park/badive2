6ca423c4625a6fa1d3f7058d234e92b5a76d782b
Lee GUKBeom <tmwlak@gmail.com>
2025-04-29 15:52:08 +0900

    [fix] 강습프로그램-> 스쿠버다이빙 : '자세히 보러가기' 항목에 링크 연결 (2)

--- app/programs/components/Strength.tsx
+++ app/programs/components/Strength.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
 
 export default function Strength() {
@@ -21,28 +22,41 @@ export default function Strength() {
                 </div>
               </div>
             </div>
-            <div className="flex aspect-[333/100] w-full max-w-[333px] flex-row items-center justify-between rounded-3xl bg-[#0053C9] p-6 text-white md:aspect-[226/200] md:max-w-[226px] md:flex-col xl:aspect-[3/2] xl:max-w-[300px]">
-              <div className="flex w-full flex-col items-start justify-start">
-                <div className="text-[15px] font-medium md:text-[20px]">가격이 부담된다면 !</div>
-                <div className="text-[20px] font-bold md:text-[30px]">무료클래스 진행 !</div>
+            <Link
+              className="w-full max-w-[333px] md:aspect-[226/200] md:max-w-[226px] xl:aspect-[3/2] xl:max-w-[300px]"
+              href="https://www.badive.co.kr/community/notification/52"
+            >
+              <div className="flex aspect-[333/100] w-full max-w-[333px] flex-row items-center justify-between rounded-3xl bg-[#0053C9] p-6 text-white md:aspect-[226/200] md:max-w-[226px] md:flex-col xl:aspect-[3/2] xl:max-w-[300px]">
+                <div className="flex w-full flex-col items-start justify-start">
+                  <div className="text-[15px] font-medium md:text-[20px]">가격이 부담된다면 !</div>
+                  <div className="text-[20px] font-bold md:text-[30px]">무료클래스 진행 !</div>
+                </div>
+                <div className="flex w-full items-center justify-end">
+                  <HiOutlineArrowNarrowRight className="text-[30px] text-white" />
+                </div>
               </div>
-              <div className="flex w-full items-center justify-end">
-                <HiOutlineArrowNarrowRight className="text-[30px] text-white" />
-              </div>
-            </div>
+            </Link>
           </div>
           <div className="mt-2 flex w-full flex-col items-center justify-between gap-y-7 md:mt-0 md:w-1/3">
-            <div className="flex aspect-[333/100] w-full max-w-[333px] flex-row items-center justify-between rounded-3xl bg-[#0053C9] p-6 text-white md:aspect-[226/200] md:max-w-[226px] md:flex-col xl:aspect-[3/2] xl:max-w-[300px]">
-              <div className="flex w-full flex-col items-start justify-start">
-                <div className="text-[15px] font-medium md:text-[20px]">체계적인 강습</div>
-                <div className="text-[20px] font-bold md:text-[30px]">전문적인 강사들 보유 </div>
+            <Link
+              className="aspect-[333/100] w-full max-w-[333px] md:aspect-[226/200] md:max-w-[226px] xl:aspect-[3/2] xl:max-w-[300px]"
+              href="https://www.badive.co.kr/instructors/bdn"
+            >
+              <div className="flex aspect-[333/100] w-full max-w-[333px] flex-row items-center justify-between rounded-3xl bg-[#0053C9] p-6 text-white md:aspect-[226/200] md:max-w-[226px] md:flex-col xl:aspect-[3/2] xl:max-w-[300px]">
+                <div className="flex w-full flex-col items-start justify-start">
+                  <div className="text-[15px] font-medium md:text-[20px]">체계적인 강습</div>
+                  <div className="text-[20px] font-bold md:text-[30px]">전문적인 강사들 보유 </div>
+                </div>
+                <div className="flex items-center justify-end md:w-full">
+                  <HiOutlineArrowNarrowRight className="text-[30px] text-white" />
+                </div>
               </div>
-              <div className="flex items-center justify-end md:w-full">
-                <HiOutlineArrowNarrowRight className="text-[30px] text-white" />
-              </div>
-            </div>
+            </Link>
           </div>
-          <div className="mt-2 flex w-full flex-col items-center justify-between gap-y-2 md:mt-0 md:w-1/3 md:gap-y-7">
+          <Link
+            href={'https://www.badive.co.kr/community/notification/50'}
+            className="mt-2 flex w-full flex-col items-center justify-between gap-y-2 md:mt-0 md:w-1/3 md:gap-y-7"
+          >
             <div className="flex aspect-[333/100] w-full max-w-[333px] flex-row items-center justify-between rounded-3xl bg-[#0053C9] p-6 text-white md:aspect-[226/200] md:max-w-[226px] md:flex-col xl:aspect-[3/2] xl:max-w-[300px]">
               <div className="flex w-full flex-col items-start justify-start">
                 <div className="text-[15px] font-medium md:text-[20px]">더욱 나아진 다이빙을 위해</div>
@@ -63,7 +77,8 @@ export default function Strength() {
                 </div>
               </div>
             </div>
-          </div>
+          </Link>
+          {/* <div className="mt-2 flex w-full flex-col items-center justify-between gap-y-2 md:mt-0 md:w-1/3 md:gap-y-7"></div> */}
         </div>
       </div>
     </div>
